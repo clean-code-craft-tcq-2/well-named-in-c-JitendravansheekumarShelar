fcb82ecb3b791194c60167b1166031d2b5bcf01a
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-01-11 23:04:16 +0530

    Update ColorCode.c
    Final Code is updated

--- ColorCode.c
+++ ColorCode.c
@@ -4,13 +4,9 @@
 #include "testNumberToPair.h"
 #include "testPairToNumber.h"
 
-
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
-
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
@@ -18,12 +14,6 @@ const char* MinorColorNames[] = {
 
 int numberOfMinorColors =
     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",
-        MajorColorNames[colorPair->majorColor],
-        MinorColorNames[colorPair->minorColor]);
-}
 
 
 int main() {
