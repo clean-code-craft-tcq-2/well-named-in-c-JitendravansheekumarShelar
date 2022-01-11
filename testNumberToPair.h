5b0d22c691ae480014fc5b785d5c0570031bab0c
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-01-11 18:11:28 +0530

    Create testNumberToPair.h

---
+++ testNumberToPair.h
@@ -0,0 +1,4 @@
+// This header file contains the declarations/prototypes defined in testNumberToPair.c file
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
