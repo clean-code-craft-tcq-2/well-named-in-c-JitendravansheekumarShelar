ba22ee0881313fb5fa4a4ea3789d6ae9de95b77e
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-01-11 23:08:49 +0530

    Update ColorPairToString.h

--- ColorPairToString.h
+++ ColorPairToString.h
@@ -1,1 +1,8 @@
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
+#ifndef TESTCPTS_H
+#define TESTCPTS_H
+
+#include "ColorCode.h"
+
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
+    
+#endif
