a28f9e159de31768dbb85341a5fd20658a2270ba
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-04 00:23:48 +0530

    Update testPrintWiringManual.c

--- testPrintWiringManual.c
+++ testPrintWiringManual.c
@@ -1,14 +1,19 @@
 #include "ColorCode.h"
 #include "GetColorFromPairNumber.h"
 #include "testPrintWiringManual.h"
+#include "ColorPairToString.h"
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
 
 /*Print Wiring Manual*/
 void testPrintWiringManual(void){
     
-    ColorPair color;    
-    for(int PairNumber = 2; PairNumber <= MAX_NUMBER_OF_PAIR; PairNumber++)
+    ColorPair color;  
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    for(int PairNumber = 1; PairNumber <= MAX_NUMBER_OF_PAIR; PairNumber++)
     {
         color = GetColorFromPairNumber(PairNumber);
+        ColorPairToString(&color, colorPairNames);
         printf("%d | %s | %s\n", PairNumber, color.majorColor, color.minorColor);
          
     }
