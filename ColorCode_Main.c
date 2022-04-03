8948d0b448d9d7a4791aa03e2bc7cd7affc3c425
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-04 00:28:17 +0530

    Update ColorCode_Main.c

--- ColorCode_Main.c
+++ ColorCode_Main.c
@@ -1,43 +1,19 @@
 #include <stdio.h>
+#include <assert.h>
 #include "ColorCode.h"
-#include "testNumberToPair.h"
-#include "testPairToNumber.h"
-#include "testPrintWiringManual.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 int main() {
-    int printManualFlag = 0;
-    
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    
-    printf("\n*********************\n");
-    printf("Do you want to print Wiring Manual on Console :\n0 : Yes\n1:No\n");
-    printf("\n*********************\n");
-    scanf("%d",&printManualFlag);
-    
-    if(printManualFlag == 0)
-    {
-        testPrintWiringManual();
-    }
-    else
-    {
-        //Do Nothing
-    }
-           
+    DisplayColorCodeManual();
     return 0;
 }
