1296b148a99d57f4c7fabeaf3a0b18542b184395
JitendravansheekumarShelar <91045255+JitendravansheekumarShelar@users.noreply.github.com>
2022-04-03 23:55:41 +0530

    Update testPrintWiringManual.c

--- testPrintWiringManual.c
+++ testPrintWiringManual.c
@@ -1,3 +1,4 @@
+#include "ColorCode.h"
 #include "GetColorFromPairNumber.h"
 #include "testPrintWiringManual.h"
 
@@ -7,7 +8,7 @@ void testPrintWiringManual(void){
     ColorPair color;    
     for(int PairNumber = 1; PairNumber <= MAX_NUMBER_OF_PAIR; PairNumber++)
     {
-        /*color = GetColorFromPairNumber(PairNumber);*/
+        color = GetColorFromPairNumber(PairNumber);
         printf("%d | %s | %s\n", PairNumber, color.majorColor, color.minorColor);
          
     }
